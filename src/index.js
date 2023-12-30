2a3c277f667525d988fc890e746c24131bb341bd
Sofia Etchegoin <sofiaetchegoin@gmail.com>
2023-12-30 15:16:33 +1300

    changed location of main.css to index.js file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
+import './styles/main.css'
 import App from './components/App'
 
 const queryClient = new QueryClient()
